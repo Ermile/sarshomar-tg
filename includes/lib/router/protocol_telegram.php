a470a548310b7035fe556f170344d977ea78be09
baravak <itb.baravak@gmail.com>
2017-03-18 15:52:49 +0330

    telegram log

--- includes/lib/router/protocol_telegram.php
+++ includes/lib/router/protocol_telegram.php
@@ -23,6 +23,7 @@ class protocol_telegram
 			echo "true";
 			exit(1);
 		}
+		file_put_contents(root . '/public_html/log.json', $string_hook);
 		$update_id = $hook['update_id'];
 		unset($hook['update_id']);
 		router::$class = key($hook);
