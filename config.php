9e3b91f25ac112ba7c0788fc9594261d339f6b71
Reza <rm.biqarar@gmail.com>
2018-04-18 17:40:38 +0430

    fix config

--- config.php
+++ config.php
@@ -7,18 +7,18 @@ define("START_TIME", microtime(true));
 // ** MySQL settings - You can get this info from your web host ** //
  /** The name of the database */
 if(!defined('db_name'))
- define("db_name", 'telegram_admin');
+ define("db_name", '__db_name__');
 
  /** MySQL database username */
 if(!defined('db_user'))
- define("db_user", 'ermile');
+ define("db_user", '__db_user__');
 
  /** MySQL database password */
 if(!defined('db_pass'))
- define("db_pass", 'Ermile@#$1233');
+ define("db_pass", '_db_pass_');
 
- define("admin_db_user", 'ermile');
+ define("admin_db_user", '__admin_db_user_');
 
- define("admin_db_pass", 'Ermile!@#456');
+ define("admin_db_pass", '__admin_db_pass_');
 
 ?>
